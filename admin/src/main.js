e58a06aca055072dea22fee7e023ed771572ac5f
Samuele Busi <KingBromeliad@users.noreply.github.com>
2021-01-14 19:16:16 +0100

    admin created

---
+++ admin/src/main.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import VueSocketIOExt from 'vue-socket.io-extended'
+import { io } from 'socket.io-client'
+import "tailwindcss/tailwind.css"
+
+const socket = io("http://localhost:3000/", {
+    withCredentials: true,
+    extraHeaders: {
+        "cors-header" : "valid"
+    }
+})
+
+Vue.use(VueSocketIOExt, socket)
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
